7ca77d80e2cd0ae146c634df0741267c578a8358
Villeman <villeman.arm@gmail.com>
2024-08-31 12:34:49 +0300

    fix: currency rates viewing

--- src/common_functions/api/converterApi.ts
+++ src/common_functions/api/converterApi.ts
@@ -6,9 +6,7 @@ export default class ConverterApi {
     protected serverURL = settings.serverURL
 
     getRates(baseCurrency: string, currencies: Currencies) {
-        // const base = 'USD'
-        // const currencies = 'RUB,EUR,USD'
-        const resolution = '1h'
+        const resolution = '1m'
         const url = `https://api.fxratesapi.com/latest?base=${baseCurrency}&currencies=${currencies.join(',')}&resolution=${resolution}&amount=1&places=6&format=json`
 
         const response = axios.get(url).catch((error) => console.log(error))
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -3,20 +3,18 @@
         v-for="(rate, currency) in converterStore.rates"
         :key="currency"
     >
-        {{ `${currency}: ${rate.toFixed(2)}` }}
+        {{
+            currency !== converterStore.baseCurrency
+                ? `${currency}: ${(1 / rate).toFixed(2)} ${converterStore.baseCurrency}`
+                : ''
+        }}
     </p>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref, computed, onMounted, onUpdated, watch } from 'vue'
-//import {useRoute} from 'vue-router'
 import { useConverterStore } from '@/stores/converterStore'
 
 const converterStore = useConverterStore()
-
-defineProps<{
-    msg?: string
-}>()
 </script>
 
 <style scoped lang="sass">
